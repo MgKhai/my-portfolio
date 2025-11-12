9c22dad4bb21abd3f452ec49c3bdba517a24ef73
MgKhai <195129227+MgKhai@users.noreply.github.com>
2025-11-12 16:13:54 +0300

    feat:hero_jsx_add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,14 @@
 const Header = () => {
+  // simple functio to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    if(mobileMenu.classList.contains('hidden')){
+      mobileMenu.classList.remove('hidden');
+    }else{
+      mobileMenu.classList.add('hidden');
+    }
+  }
   return (
     <header className="flex justify-between items-center py-4 px-4 lg:px-20">
 
@@ -32,6 +42,36 @@ const Header = () => {
       <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor-pointer z-50">
         SIGNIN
       </button>
+
+      {/* Mobile menu button - visible only on Mobile */}
+      <button onClick={toggleMobileMenu}  className="md:hidden text-3xl p-2 z-50">
+        <i class="bx bx-menu"></i>
+      </button>
+
+      {/* Mobile menu - Hidden by default */}
+      <div id="mobileMenu" className="hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur- md">
+        <nav className="flex flex-col gap-6 items-center">
+        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#"> 
+            Home
+          </a>
+
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#"> 
+            About
+          </a>
+
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#"> 
+            Projects
+          </a>
+
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#"> 
+            Experience
+          </a>
+
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#"> 
+            Contact
+          </a>
+        </nav>
+      </div>
 
     </header>
   )
