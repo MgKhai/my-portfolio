cbb98422ae198e23a7038a51d4208af79e4ad505
MgKhai <195129227+MgKhai@users.noreply.github.com>
2025-11-17 16:33:37 +0300

    feat:nav_bar_create

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,52 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  webpack(config) {
+    
+    const fileLoaderRule = config.module.rules.find((rule:any) =>
+      rule.test?.test?.(".svg")
+    );
+
+    
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, 
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [/url/] }, 
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      }
+    );
+
+    
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+
+  turbopack: {},
 };
 
 export default nextConfig;
