2eeb3f78147e77d6e04742e3b581dd28d4436b70
MgKhai <195129227+MgKhai@users.noreply.github.com>
2025-12-16 02:19:25 +0630

    Fix: Corrected file casing for Expertise and Projects sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero"
-import { ProjectsSection } from "@/sections/Projects";
+import { ProjectsSection } from "@/sections/projects";
 import { TapeSection } from "@/sections/Tape";
-import { ExpertiseSection } from "@/sections/Expertise";
+import { ExpertiseSection } from "@/sections/expertise";
 import { ToolkitSection } from "@/sections/Toolkit";
 import { AcademicSection } from "@/sections/Academic";
 import { ContactSection } from "@/sections/Contact";
